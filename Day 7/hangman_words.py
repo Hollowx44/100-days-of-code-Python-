9c4ca238c9f8ce94515a836036b62f319171e29f
Hollowx44 <adityakukshal44@gmail.com>
2025-07-17 17:23:20 +0530

    project update

---
+++ Day 7/hangman_words.py
@@ -0,0 +1,7 @@
+word_list = [
+    "United States", "India", "China", "Japan", "United Kingdom", "France", "Germany", "Italy", "Canada", "South Korea",
+    "Russia", "Brazil", "Australia", "Mexico", "Spain", "Thailand", "Turkey", "Indonesia", "Switzerland", "Netherlands",
+    "Saudi Arabia", "UAE", "Singapore", "South Africa", "Egypt", "Vietnam", "Greece", "Malaysia", "Argentina", "Philippines",
+    "Sweden", "Portugal", "Pakistan", "New Zealand", "Israel", "Ukraine", "Poland", "Norway", "Bangladesh", "Belgium",
+    "Ireland", "Denmark", "Qatar", "Iran", "Colombia", "Morocco", "Finland", "Iraq", "Czech Republic", "Chile"
+]
