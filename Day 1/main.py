bc48c98edd090e618c8db19c5dff230f7a57c38a
Hollowx44 <adityakukshal44@gmail.com>
2025-07-17 09:45:42 +0530

    Add new file

---
+++ Day 1/main.py
@@ -0,0 +1,8 @@
+welcome = "Welcome to the Band Name Generator"
+print(welcome)
+
+city = input("What's the name of the city you grew up in?\n").capitalize()
+
+food = input("What's the last thing you ate? \n").capitalize()
+
+print("Your band name could be " + city + " " + food)
