a764804453cbd7b2a0860b9d418a59ab40817a3c
Hollowx44 <adityakukshal44@gmail.com>
2025-07-20 17:20:15 +0530

    Add version 1.1 of project:BlackJack

--- Day11/main.py
+++ Day11/main.py
@@ -1,59 +1,63 @@
 import random
-def endgame():
-    while True:
-        pass
-cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
-dealer_sum = 0
-player_sum = 0
-p_cards = []
-d_cards = []
-d_data = random.choice(cards)
-d_cards.append(d_data)
-dealer_sum += d_data
-for i in range(2):
-    p_data = random.choice(cards)
-    p_cards.append(p_data)
-    player_sum += p_data
-p_choice = True
-print(f"Player's Hand: {p_cards}  ||  Dealer's Hand: {d_cards}")
-while p_choice:
-    way = input('Y for Hit  ||  N for Stand\nPLayer Chose:').lower()
-    if way == 'y':
-        p_data = random.choice(cards)
-        p_cards.append(p_data)
-        player_sum += p_data
-        print(f"Player Card: {p_data}   ||  Player's Hand: {p_cards} ")
-        if player_sum > 21:
-            print('You loose')
-            endgame()
-        elif player_sum == 21:
-            print('BLACKJACK ----> You Win')
-            endgame()
-
-    elif way == 'n':
-        print('Player chose Stand')
-        p_choice = False
-
-d_choice = True
-while d_choice:
+def BlackJack():
+    cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+    dealer_sum = 0
+    player_sum = 0
+    p_cards = []
+    d_cards = []
     d_data = random.choice(cards)
     d_cards.append(d_data)
     dealer_sum += d_data
-    print("Dealer on Hit")
-    print(f"Dealer Card: {d_data}   ||  Dealer's Hand: {d_cards} ")
-    if dealer_sum > 21:
-        print("Dealer loose")
-        endgame()
-    elif dealer_sum==21:
-        print("BLACKJACK ---> Dealer win")
-        endgame()
-    elif dealer_sum >= 17:
-        print("Dealer on Stand")
-        d_choice = False
+    for i in range(2):
+        p_data = random.choice(cards)
+        p_cards.append(p_data)
+        player_sum += p_data
+    p_choice = True
+    print(f"Player's Hand: {p_cards}  ||  Dealer's Hand: {d_cards}\nPlayer's Score: {player_sum}\n")
+    while p_choice:
+        way = input('"Y" for Hit  ||  "N" for Stand\nPLayer Chose:').lower()
+        if way == 'y':
+            p_data = random.choice(cards)
+            p_cards.append(p_data)
+            player_sum += p_data
+            print(f"Player Card: {p_data}   ||  Player's Hand: {p_cards}  ||  Player's Score: {player_sum}\n")
+            if player_sum > 21:
+                print('You loose')
+                return
+            elif player_sum == 21:
+                print('BLACKJACK ----> You Win')
+                return
 
-if dealer_sum == player_sum:
-    print("DRAW!!")
-elif dealer_sum > player_sum:
-    print("Dealer Win")
-else:
-    print("Player Win")
+        elif way == 'n':
+            print('Player chose Stand\n')
+            p_choice = False
+
+    d_choice = True
+    while d_choice:
+        d_data = random.choice(cards)
+        d_cards.append(d_data)
+        dealer_sum += d_data
+        if dealer_sum > 21:
+            print(f"Dealer loose -----> Dealer's Score: {dealer_sum}")
+            return
+        elif dealer_sum==21:
+            print(f"BLACKJACK ---> Dealer win ----> Dealer's score: {dealer_sum}")
+            return
+        elif dealer_sum >= 17:
+            print(f"Dealer on Stand  || Dealer's Hand: {d_cards}  ||  Dealer's Score: {dealer_sum}\n")
+            d_choice = False
+
+    if dealer_sum == player_sum:
+        print("DRAW!!")
+    elif dealer_sum > player_sum:
+        print(f"Dealer Win -----> Player's Score: {player_sum}")
+    else:
+        print(f"Player Win ------> Player's Score: {player_sum}")
+while True:
+    choice=input("Want to play a game of BlackJack (Y/N): ").lower()
+    if choice == "y":
+        BlackJack()
+    elif choice == "n":
+        break
+    else:
+        print("Enter valid choice!!")
